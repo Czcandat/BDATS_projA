9f7c6566ba03d843c92cea4d99bfb8bd5250ddee
Czcandat <zdenek.stastny@centrum.cz>
2024-10-14 16:40:34 +0200

    working load and visual

--- src/main/java/me/bdats_proja/Controller.java
+++ src/main/java/me/bdats_proja/Controller.java
@@ -1,14 +1,26 @@
 package me.bdats_proja;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class Controller implements Initializable
 {
@@ -22,7 +34,6 @@ public class Controller implements Initializable
         }
     }
 
-
     @FXML
     private Label Load;
 
@@ -32,12 +43,75 @@ public class Controller implements Initializable
         FileChooser fileChooser = new FileChooser();
         File file = fileChooser.showOpenDialog(new Stage());
         int successfullyLoaded = Obyvatele.importData(String.valueOf(file));
+        refreshTabs();
 
     }
+
+    @FXML
+    private TabPane tabPane;
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
         innitKraje();
+        for (int i = 0; i < tabPane.getTabs().size(); i++)
+        {
+            Tab tab = tabPane.getTabs().get(i);
+            TableView<Obec> tableView = createTableView();
+
+            // Convert the custom list to an ObservableList
+            ObservableList<Obec> observableList = convertToObservableList(kraje[i]);
+            tableView.setItems(observableList);
+
+            tab.setContent(tableView);
+        }
+    }
+
+    private ObservableList<Obec> convertToObservableList(AbstrDoubleList<Obec> list) {
+        ObservableList<Obec> observableList = FXCollections.observableArrayList();
+        for (Obec obec : list) {  // Assuming your AbstrDoubleList implements Iterable
+            observableList.add(obec);
+        }
+        return observableList;
+    }
+
+    private TableView<Obec> createTableView()
+    {
+        TableView<Obec> tableView = new TableView<>();
+
+        TableColumn<Obec, Integer> pscColumn = new TableColumn<>("PSC");
+        pscColumn.setCellValueFactory(new PropertyValueFactory<>("psc"));
+
+        TableColumn<Obec, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Obec, Integer> muziPocetColumn = new TableColumn<>("Muzi");
+        muziPocetColumn.setCellValueFactory(new PropertyValueFactory<>("muziPocet"));
+
+        TableColumn<Obec, Integer> zenyPocetColumn = new TableColumn<>("Zeny");
+        zenyPocetColumn.setCellValueFactory(new PropertyValueFactory<>("zenyPocet"));
+
+        TableColumn<Obec, Integer> celkemPocetColumn = new TableColumn<>("Celkem");
+        celkemPocetColumn.setCellValueFactory(new PropertyValueFactory<>("celkemPocet"));
+
+        tableView.getColumns().addAll(pscColumn, nameColumn, muziPocetColumn, zenyPocetColumn, celkemPocetColumn);
+
+        return tableView;
+
+
+    }
+    public void refreshTabs() {
+        for (int i = 0; i < tabPane.getTabs().size(); i++) {
+            Tab tab = tabPane.getTabs().get(i);
+            TableView<Obec> tableView = createTableView();
+
+            // Convert the custom list to an ObservableList
+            ObservableList<Obec> observableList = convertToObservableList(kraje[i+1]);
+            tableView.setItems(observableList);
+
+            tab.setContent(tableView);
+        }
     }
 }
--- src/main/java/me/bdats_proja/Obec.java
+++ src/main/java/me/bdats_proja/Obec.java
@@ -1,19 +1,27 @@
 package me.bdats_proja;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Obec
 {
-    public int psc;
-    public String obec;
-    public int muziPocet;
-    public int zenyPocet;
-    public int celkemPocet;
+    private SimpleIntegerProperty psc;
+    private SimpleStringProperty name;
+    private SimpleIntegerProperty muziPocet;
+    private SimpleIntegerProperty zenyPocet;
+    private SimpleIntegerProperty celkemPocet;
 
-    Obec (int psc, String obec, int muziPocet, int zenyPocet, int celkemPocet)
-    {
-        this.psc = psc;
-        this.obec = obec;
-        this.muziPocet = muziPocet;
-        this.zenyPocet = zenyPocet;
-        this.celkemPocet = celkemPocet;
+    public Obec(int psc, String name, int muziPocet, int zenyPocet, int celkemPocet) {
+        this.psc = new SimpleIntegerProperty(psc);
+        this.name = new SimpleStringProperty(name);
+        this.muziPocet = new SimpleIntegerProperty(muziPocet);
+        this.zenyPocet = new SimpleIntegerProperty(zenyPocet);
+        this.celkemPocet = new SimpleIntegerProperty(celkemPocet);
     }
+
+    public int getPsc() { return psc.get(); }
+    public String getName() { return name.get(); }
+    public int getMuziPocet() { return muziPocet.get(); }
+    public int getZenyPocet() { return zenyPocet.get(); }
+    public int getCelkemPocet() { return celkemPocet.get(); }
 }

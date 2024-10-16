c9eaf18760d9ec748d3663f828ab23ac3c070372
Czcandat <zdenek.stastny@centrum.cz>
2024-10-16 02:01:54 +0200

    structure changes

--- src/main/java/me/bdats_proja/ProgObyvatele.java
+++ src/main/java/me/bdats_proja/ProgObyvatele.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ProgObyvatele extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ProgObyvatele.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("BDATS_projA");
         stage.setScene(scene);
